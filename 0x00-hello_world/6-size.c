40b4d366223719ea81fdc1d5aeae58d5053e2eb1
Lelia Lelia <nelledia1@gmail.com>
2022-12-08 11:22:35 -0800

    changes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,11 +11,13 @@ int main(void)
 	float floatType;
 	double doubleType;
 	char charType;
+	long double longdoubleType;
 
-	printf("Size of int: %zu bytes\n", sizeof(intType));
-	printf("Size of float: %zu bytes\n", sizeof(floatType));
-	printf("Size of double: %zu bytes\n", sizeof(doubleType));
 	printf("Size of char: %zu bytes\n", sizeof(charType));
+	printf("Size of an int: %zu bytes\n", sizeof(intType));
+	printf("Size of a long int: %zu bytes\n", sizeof(doubleType));
+	printf("Size of a long long int: %zu bytes\n", sizeof(longdoubleType));
+	printf("Size of float: %zu bytes\n", sizeof(floatType));
 
 	return (0);
 }

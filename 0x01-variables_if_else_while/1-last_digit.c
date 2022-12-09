b60b22f5667b834141504949782e684cb35903fd
Lelia Lelia <nelledia1@gmail.com>
2022-12-09 07:12:23 -0800

    Alphabet

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,5 +23,5 @@ int main(void)
 		printf("Last digit of %d is %d and is 0\n", n, last);
 	else
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
-return (0);
+	return (0);
 }
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+/**
+ * main - Entry point
+ *
+ * return - Always 0 (success)
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		putchar(alphabet);
+		putchar('\n');
+
+	return (0);
+}

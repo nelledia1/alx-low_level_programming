803018dddff1993b6ac73a55a0f85fec6fc04d17
Lelia Lelia <nelledia1@gmail.com>
2022-12-13 08:24:44 -0800

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10:The function that uses putchar to print alphabet 10 times
+ * Description: Print alphabet 10 times in lower case
+ * Return: Nothing.
+ */
+
+void print_alphabet_x10(void)
+{
+	int n;
+	char i;
+
+	for (n = 0; n <= 9; n++)
+	{
+		for(i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}

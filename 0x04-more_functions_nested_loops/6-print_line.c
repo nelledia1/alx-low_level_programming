a8ff37bdd5bdcdb921b741a2645c29466a173a93
Lelia Lelia <nelledia1@gmail.com>
2022-12-15 08:59:02 -0800

    The shortest distance

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - a function that draws a straight line in the terminal.
+ * @n: the number of times the character _ should be printed
+ * Description: use if statement and for loop
+ * Return: Always 0
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}

4ff9cf3733e870182488ef0206c0e3b783bba95b
Lelia Lelia <nelledia1@gmail.com>
2022-12-19 09:13:12 -0800

    parameter description

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,10 +3,10 @@
 /**
  * reset_to_98 - A function that takes a pointer to an int as parameter and
  * updates the value it points to to 98
- * *n: pointer variable
+ * @n: pointer variable
  * Description: sets *n to 98
  * Return: Nothing
- * */
+ */
 
 void reset_to_98(int *n)
 {

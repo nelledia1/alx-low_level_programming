2dfa6d3f0c02454c03fc9d03ca65418aa025b502
Lelia Lelia <nelledia1@gmail.com>
2022-12-19 09:08:58 -0800

    changes

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,8 +3,8 @@
 /**
  * reset_to_98 - A function that takes a pointer to an int as parameter and
  * updates the value it points to to 98
- * *n: point variable
- * Description: 
+ * *n: pointer variable
+ * Description: sets *n to 98 
  * Return: Nothing
  */
 

a094f0582f94026fd5a17fa3ccaf065d84313af6
Lelia Lelia <nelledia1@gmail.com>
2022-12-19 09:10:48 -0800

    delete trailing whitespace

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,9 +4,9 @@
  * reset_to_98 - A function that takes a pointer to an int as parameter and
  * updates the value it points to to 98
  * *n: pointer variable
- * Description: sets *n to 98 
+ * Description: sets *n to 98
  * Return: Nothing
- */
+ * */
 
 void reset_to_98(int *n)
 {

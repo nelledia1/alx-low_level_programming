4af420bd819a6fd6152fba7da28dd61736b3c56d
Lelia Lelia <nelledia1@gmail.com>
2022-12-20 11:19:16 -0800

    changes

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * print_rev - Function that prints a string, in reverse, followed by a new line
+ * print_rev - Function that prints a string, in reverse,
+ * followed by a new line
  * @s: sting input
  * Description: use for loop
  * Return: Always 0
@@ -10,16 +11,18 @@
 void print_rev(char *s)
 {
 	int length = 0;
-	
-	while (s[length] != '\0')
+	int t;
+
+	while (*s != '\0')
 	{
 		length++;
-		{
-			while (length);
-			{
-				_putchar(s[--length]);
-			}
-		}
+		s++;
+	}
+	s--;
+	for (t = length; t > 0; t--)
+	{
+		_putchar(*s);
+		s--;
 	}
 	_putchar('\n');
 }

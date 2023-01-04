d994b86c94c167c5975a4765b374fc0cd3c92891
Lelia Lelia <nelledia1@gmail.com>
2023-01-04 10:17:50 -0800

    Task 2

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - A function that prints a string in reverse
+ * @s: character parameter
+ * Return: Nothing
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
+}
+
+
